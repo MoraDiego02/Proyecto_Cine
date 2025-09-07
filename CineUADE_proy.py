8769f01a026b4cdbe8a4507eca4fe13af580f258
chupete <lautaropiston.lp@gmail.com>
2025-09-07 17:52:44 -0300

    creaciondesalas

--- CineUADE_proy.py
+++ CineUADE_proy.py
@@ -1,3 +1,7 @@
+from funciones_cine import MostrarSala
+from funciones_cine import crearsala
+
+
 def inicio():
     print("-" * 40)
     print("|      🎥 Bienvenido a CineUADE 🎥      |")
@@ -11,6 +15,22 @@ def inicio():
 
 def main():
     print("CINEUADE")
+    print("bienvenidos al cien uade")
+    print("en que sucursal quiere ver la pelicula")
+    print("ingrese 1 un seleccionar")
+    sucursal= 1 #int(input("ingrese un numero para seleccionar la sucursal"))
+    print("elija un numero del 1 al 3")
+    print("si quiere volver a la pagina principal ingrese -1")
+    NumeroSala= 1
+    while NumeroSala >= 1 or NumeroSala <= 3:
+        NumeroSala=int(input("ingrese el numero de sala que quiere seleccionar "))
+        while NumeroSala != -1:
+            MostrarSala(crearsala(NumeroSala))
+            print("que asiento quiere seleccionar para esta pelicula")
+            FilaAsiento=int(input("seleccione la fila en la que quiere su asiento"))
+            ColumnaAsiento=int(input("seleccione la columna en la que quiere su asiento"))
+            NumeroSala= -1 # int(input("ingrese el numero de sala que quiere seleccionar -1 para salir de esto:"))
+
 
 if __name__ == "__main__":
     inicio()
--- funciones_cine.py
+++ funciones_cine.py
@@ -1,4 +1,44 @@
 import random
+
+def crearsala(numero,sucursal,SalaCreadas):
+    columnas=5
+    filas=5
+    if 
+    if  sucursal == 1 and numero == 1:
+        for i in range()
+        Sala_1=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+    else:
+        return sala_1
+    
+    if sucursal == 1 and numero == 2:
+        Sala_2=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_2
+    if sucursal == 1 and numero == 3:
+        Sala_3=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_3
+
+    if sucursal == 2 and numero == 1:
+        Sala_4=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_4
+    if sucursal == 2 and numero == 2:
+        Sala_5=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_5
+    if sucursal == 2 and numero == 3:
+        Sala_6=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_6
+
+    if sucursal == 3 and numero == 1:
+        Sala_7=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_7
+    if sucursal == 3 and numero == 2:
+        Sala_8=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_8
+    if  sucursal == 3 and numero == 3:
+        Sala_9=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
+        return Sala_9
+
+
+
 def MostrarSala(matriz):
     columnas=len(matriz[1])
     print("        ",end="")
@@ -13,38 +53,62 @@ def MostrarSala(matriz):
             else:
                 print(" ".ljust(5),end="    ")
         print()
+    return
 
-def busq_mat(matrix,target): #buscar en matriz
-    total_fila=len(matrix)
-    total_columnas=len(matrix[0])
-    mini,maxi=0,(total_columnas*total_fila)-1
-    while mini<=maxi:
-        mid= (mini+maxi)//2
-        fila=mid//total_fila
-        col=mid%total_columnas
+print("bienvenidos al cien uade")
+print("en que sucursal quiere ver la pelicula")
+print("ingrese 1 un seleccionar")
+sucursal= 1 #int(input("ingrese un numero para seleccionar la sucursal"))
+print("elija un numero del 1 al 3")
+print("si quiere volver a la pagina principal ingrese -1")
+NumeroSala= 1
+while NumeroSala >= 1 and NumeroSala <= 3:
+    NumeroSala=int(input("ingrese el numero de sala que quiere seleccionar "))
+    while NumeroSala != -1:
+        MostrarSala(crearsala(NumeroSala))
+        print("que asiento quiere seleccionar para esta pelicula")
+        FilaAsiento=int(input("seleccione la fila en la que quiere su asiento"))
+        ColumnaAsiento=int(input("seleccione la columna en la que quiere su asiento"))
+        NumeroSala= -1 # int(input("ingrese el numero de sala que quiere seleccionar -1 para salir de esto:"))
 
-        if matrix[fila][col] == target:
-            return True
+def nosecomoponer(): 
+    if sucursal == 1 and NumeroSala == 1:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 2:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 3:
+        BuscarAsiento(crearsala(NumeroSala))
 
-        
-        elif matrix[fila][col] < target:
-            mini= mid+1
+    if sucursal == 1 and NumeroSala == 1:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 2:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 3:
+        BuscarAsiento(crearsala(NumeroSala))
 
-        else:
-            maxi= mid-1
-    return False
+    if sucursal == 1 and NumeroSala == 1:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 2:
+        BuscarAsiento(crearsala(NumeroSala))
+    if sucursal == 1 and NumeroSala == 3:
+        BuscarAsiento(crearsala(NumeroSala))
 
 
-def crearsala(numero):#crearsala 
-    columnas=5
-    filas=5
-    if numero == 1:
-        Sala_1=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
-        return Sala_1
-    if numero == 2:
-        Sala_2=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
-        return Sala_2
-    if numero == 3:
-        Sala_3=[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]
-        return Sala_3
-    
+"""
+def PagarLosAsiento():
+    for i in range():
+    if 
+    print("el asiento esta disponible")
+
+
+
+def reportardinerodiario():
+    Preciopelicula=7.084,93"""
+
+
+
+if __name__ == "__main__":
+    CrearSala()
+
+
+
