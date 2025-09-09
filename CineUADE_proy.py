d55b5cddb4528268527c06c3f2006d0b81d96f3a
chupete <lautaropiston.lp@gmail.com>
2025-09-09 13:56:06 -0300

    menu

--- CineUADE_proy.py
+++ CineUADE_proy.py
@@ -9,23 +9,38 @@ def inicio():
     print("-" * 40)
 
 def main():
-    print("CINEUADE")
-    print("bienvenidos al cien uade")
-    print("en que sucursal quiere ver la pelicula")
-    print("")
-    sucursal= 1 #int(input("ingrese un numero para seleccionar la sucursal"))
-    print("elija un numero del 1 al 3")
-    print("si quiere volver a la pagina principal ingrese -1")
-    NumeroSala= 1
-    while NumeroSala >= 1 or NumeroSala <= 3:
-        NumeroSala=int(input("ingrese el numero de sala que quiere seleccionar "))
-        while NumeroSala != -1:
-            MostrarSala(crearsala(NumeroSala))
-            print("que asiento quiere seleccionar para esta pelicula")
-            FilaAsiento=int(input("seleccione la fila en la que quiere su asiento"))
-            ColumnaAsiento=int(input("seleccione la columna en la que quiere su asiento"))
-            NumeroSala= -1 # int(input("ingrese el numero de sala que quiere seleccionar -1 para salir de esto:"))
 
+    Menu=0
+    while Menu==0:
+        print("bienvenidos al cine uade")
+        print("1. para comprar una entrada para el cine")
+        print("2.")
+        print("3.")
+        Opciones= 1 #int(input("ingrese el numero de la opcion que quiere (1-3) "))
+        while Opciones > 3 or Opciones < 1: 
+            print("el numero tiene que estar en un rango de 1 a 3")
+            Opciones=int(input("ingrese el numero de la opcion que quiere (1-3) "))
+
+        if Opciones == 1:#esta misma base va en las de abajo
+            print("se selecciono la opcion 1 compra entrada")
+            print("ingresa -1 para volver para atras")
+            print("")
+            Preguntar = int(input("quieres volver al menu"))
+            while 1 != Preguntar != -1:
+                print("-1")
+                Preguntar = int(input("quieres volver al menu"))
+            if Preguntar == -1:
+                print("moris")
+                Menu=0
+            else:
+                CargarSucursales()
+        if Opciones == 2:
+            print("selecciono opcion 2")
+            Menu=0
+        if Opciones == 3:
+            print("selecciono opcion 3")
+            if Opciones == -1:
+                Menu=0
 
 if __name__ == "__main__":
     inicio()
