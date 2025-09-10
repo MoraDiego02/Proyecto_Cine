8899b429d5f46caa123ef85135a27e208f63faaf
MoraDiego02 <jayhz2006@gmail.com>
2025-09-09 23:43:09 -0300

    Prueba

--- CineUADE_proy.py
+++ CineUADE_proy.py
@@ -1,11 +1,39 @@
-from funciones_cine import MostrarSala, cartelera, CargarSucursales, SeleccionarSucursal, ReservaDeButacas, PrecioDelaEntrada, comprobante, formato, FinDelDia
-import random
+from funciones_cine import cartelera, formato, CargarSucursales, SeleccionarSucursal, PrecioDelaEntrada, comprobante, FinDelDia
 
 def inicio():
     print("-" * 40)
     print("|      🎥 Bienvenido a CineUADE 🎥      |")
     print("-" * 40)
 
+<<<<<<< HEAD
+def main(sucursales):
+    dni = int(input("Ingrese su DNI: "))
+    while dni < 10000000 or dni > 99999999:
+        dni = int(input("DNI inválido. Ingrese su DNI nuevamente: "))
+    print(" ")
+    cartelera()
+    print(" Seleccione la película que desea ver ")
+    pelicula = int(input("Ingrese el número de la película: "))
+    while pelicula < 1 or pelicula > 4:
+        pelicula = int(input("Número inválido. Ingrese el número de la película nuevamente: "))
+    print(" ")
+    print("Usted seleccionó la película:", pelicula)
+    print(" ")
+    formato()
+    print(" ")
+    print("Seleccione la sucursal y Sala")
+    sucursal, sala = SeleccionarSucursal(sucursales)
+    print("Precio de la entrada")
+    PrecioDelaEntrada()
+    print(" ")
+    print("Gracias por su compra. ¡Disfrute la película!")
+    print(" ")
+    volver_a_comprar = input("¿Desea realizar otra compra? (si/no): ").lower()
+    if volver_a_comprar == "si":
+        main(sucursales)
+    else:
+        FinDelDia(sucursales)
+=======
 def main():
     Seguir = "si"
     Sucursales = CargarSucursales()
@@ -39,7 +67,9 @@ def main():
         
     print("¡Hasta luego!")      
     FinDelDia(Sucursal)
+>>>>>>> 0a2d3dde9ada683f00e72c6d5abe8d44ccfb0eee
 
 if __name__ == "__main__":
     inicio()
-    main()
+    sucursales = CargarSucursales()
+    main(sucursales)
--- funciones_cine.py
+++ funciones_cine.py
@@ -1,36 +1,33 @@
 import random
 
 #precio de la entrada
-def PrecioDelaEntrada(simular):
+def PrecioDelaEntrada():
     Dias=['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
-    GenerarDia = lambda: random.randint(0,6)
+    GenerarDia = lambda : random.randint(0,6)
     QueDiaEs=GenerarDia()
-    if simular == 0:
-        entrada=7500
-        if 0<= QueDiaEs <= 3 :
-            PrecioFinal=entrada*0.80
-            print("como hoy es",Dias[QueDiaEs],"la entrada tiene un descuento del 20%!")
-            print("la entrada para la pelicula esta:",PrecioFinal)
-        else:
-            PrecioFinal=entrada*1.10
-            print("como hoy es",Dias[QueDiaEs],"la entrada tiene un aumento del 10%")
-            print("la entrada para la pelicula esta:",PrecioFinal)
-        print("con que quiere pagar ")
-        print("ingrese 1 para pagar con tarjeta ")
-        print("ingrese 2 para abonar en efectivo ")
-        MetodoDePago=int(input("ingrese el numero (1 o 2) "))
-        while 1 != MetodoDePago != 2:
-            MetodoDePago=int(input("porfavor ingrese un numero del 1 al 2 "))
-        if MetodoDePago == 1:
-            print("se selecciono tarjeta hay un 5% de recargo para este metodo de pago")
-            PrecioFinal=PrecioFinal*1.05
-            print("el precio final seria de:",PrecioFinal)
-            return PrecioFinal
-        else: 
-            print("selecciono efectivo")
-            return PrecioFinal
-
-
+    entrada=7500
+    if 0<= QueDiaEs <= 3 :
+        PrecioFinal=entrada*0.80
+        print("como hoy es",Dias[QueDiaEs],"la entrada tiene un descuento del 20%!")
+        print("la entrada para la pelicula esta:",PrecioFinal)
+    else:
+        PrecioFinal=entrada*1.10
+        print("como hoy es",Dias[QueDiaEs],"la entrada tiene un aumento del 10%")
+        print("la entrada para la pelicula esta:",PrecioFinal)
+    print("con que quiere pagar ")
+    print("ingrese 1 para pagar con tarjeta ")
+    print("ingrese 2 para abonar en efectivo ")
+    MetodoDePago=int(input("ingrese el numero (1 o 2) "))
+    while MetodoDePago != 1 and MetodoDePago != 2:
+        MetodoDePago=int(input("porfavor ingrese un numero del 1 al 2 "))
+    if MetodoDePago == 1:
+        print("se selecciono tarjeta hay un 5% de recargo para este metodo de pago")
+        PrecioFinal=PrecioFinal*1.05
+        print("el precio final seria de:",PrecioFinal)
+        return PrecioFinal
+    else: 
+        print("selecciono efectivo")
+        return PrecioFinal
 
 def MostrarSala(matriz):
     columnas=len(matriz[0])
@@ -57,26 +54,40 @@ def CargarSucursales():
     SucursalAbasto=[[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]for i in range(3)]
     SucursalCaballito=[[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]for i in range(3)]
     SucursalPalermo=[[[random.randint(0,1)for i in range(columnas)]for i in range(filas)]for i in range(3)]
-    
-    return [SucursalAbasto,SucursalCaballito,SucursalPalermo]
+    sucursal=[SucursalAbasto,SucursalCaballito,SucursalPalermo]
+    return sucursal
 
 
+<<<<<<< HEAD
+def SeleccionarSucursal(Sucursal):
+    SucursalAbasto = Sucursal[0]
+    SucursalCaballito = Sucursal[1]
+    SucursalPalermo = Sucursal[2]
+=======
 def SeleccionarSala(Sucursal):
     SucursalAbasto=Sucursal[0]
     SucursalCaballito=Sucursal[1]
     SucursalPalermo=Sucursal[2]
+>>>>>>> 0a2d3dde9ada683f00e72c6d5abe8d44ccfb0eee
     print("1. Para la sucursal de el Abasto")
     print("2. Para la sucursal de Palermo")
     print("3. Para la sucursal de Caballito")
-    NumeroDeSucursal=int(input("ingrese el numero de la sucursal desea seleccionar: "))-1
+    NumeroDeSucursal = int(input("ingrese el numero de la sucursal desea seleccionar: ")) - 1
     while NumeroDeSucursal < 0 or NumeroDeSucursal > 2:
-        NumeroDeSucursal=int(input("ingrese el numero de sala que desea seleccionar: "))-1
+        NumeroDeSucursal = int(input("ingrese el numero de sucursal válido (1 a 3): ")) - 1
 
-    NumeroDeSala=int(input("ingrese el numero de sala que desea seleccionar (1 a 3) "))-1
+    NumeroDeSala = int(input("ingrese el numero de sala que desea seleccionar (1 a 3): ")) - 1
     while NumeroDeSala < 0 or NumeroDeSala > 2:
-        NumeroDeSala=int(input("ingrese el numero de sala que desea seleccionar (1 a 3) "))-1
+        NumeroDeSala = int(input("ingrese el numero de sala válido (1 a 3): ")) - 1
 
     if NumeroDeSucursal == 0:
+<<<<<<< HEAD
+        sala_matriz = SucursalAbasto[NumeroDeSala]
+    elif NumeroDeSucursal == 1:
+        sala_matriz = SucursalPalermo[NumeroDeSala]
+    elif NumeroDeSucursal == 2:
+        sala_matriz = SucursalCaballito[NumeroDeSala]
+=======
         for i in range(len(SucursalAbasto[0])):
             if NumeroDeSala == i:
                 return SucursalAbasto[0][i]
@@ -90,10 +101,16 @@ def SeleccionarSala(Sucursal):
         for i in range(len(SucursalCaballito[0])):
             if NumeroDeSala == i:
                 return SucursalCaballito[0][i]
+>>>>>>> 0a2d3dde9ada683f00e72c6d5abe8d44ccfb0eee
 
+    ReservaDeButacas(MostrarSala(sala_matriz))
+    return NumeroDeSucursal, NumeroDeSala
 
 
 def ReservaDeButacas(Sala):
+<<<<<<< HEAD
+    ButacasVacias = 0
+=======
     print("1. Para la sucursal de el Abasto")
     print("2. Para la sucursal de Palermo")
     print("3. Para la sucursal de Caballito")
@@ -106,47 +123,58 @@ def ReservaDeButacas(Sala):
         NumeroDeSala=int(input("ingrese el numero de sala que desea seleccionar (1 a 3) "))-1
 
     ButacasVacias=0
+>>>>>>> 0a2d3dde9ada683f00e72c6d5abe8d44ccfb0eee
     for i in range(len(Sala)):
         for j in range(len(Sala[0])):
             if Sala[i][j] == 0:
-                ButacasVacias+=1
+                ButacasVacias += 1
 
-    NumeroDeButacas=int(input("ingrese el numero de la cantidad de butacas que quiere comprar: "))
+    print()
+    print("Butacas disponibles:", ButacasVacias)
+    print()
+
+    NumeroDeButacas = int(input("¿Cuántas butacas desea comprar?: "))
     while NumeroDeButacas > ButacasVacias or NumeroDeButacas <= 0:
         if NumeroDeButacas <= 0:
-            print("ingrese un numero mayor que",NumeroDeButacas,"porfavor")
-            NumeroDeButacas=int(input("ingrese el numero de la cantidad de butacas que quiere comprar: "))
+            print(" Ingrese un número mayor que 0, por favor.")
         else:
-            print("no hay suficientes butacas vacias")
-            print("la cantidad de butacas vacias es:",ButacasVacias,"porfavor ingrese un numero menor o igual a que este")
-            NumeroDeButacas=int(input("ingrese el numero de la cantidad de butacas que quiere comprar: "))
-    while NumeroDeButacas>=1:
-        FilaDeLaButaca=0
-        ColumnaDeLaButaca=0
-        print("a continuacion seleccione la fila en la que quiere su asiento")
-        FilaDeLaButaca=int(input("ingrese un numero del 1 al 5 "))-1
-        while FilaDeLaButaca > 4 or FilaDeLaButaca < 0:
-            print("porfavor ingrese un numero del 1 al 5")
-            ColumnaDeLaButaca=int(input("ingrese un numero del 1 al 5 "))-1
-        print("ahora seleccione en que lugar de la columna le gustaria sentarse")
-        ColumnaDeLaButaca=int(input("ingrese un numero del 1 al 5 "))-1
-        while ColumnaDeLaButaca > 4 or ColumnaDeLaButaca < 0:
-            print("porfavor ingrese un numero del 1 al 5")
-            ColumnaDeLaButaca=int(input("ingrese un numero del 1 al 5 "))-1
-        
-        print(FilaDeLaButaca)
-        print(Sala)
-        for i in range(len(Sala)):
-            for j in range(len(Sala[0])):
-                if i == FilaDeLaButaca and j == ColumnaDeLaButaca:
-                    if Sala[i][j] == 0:
-                        print("el asiento esta libre ")
-                        print("ya lo reservamos para usted")
-                        Sala[i][j] = 1
-                        NumeroDeButacas-=1
-                    else:
-                        print("el asiento esta ocupado porfavor eliga otro")                
-    return FilaDeLaButaca,ColumnaDeLaButaca
+            print("No hay suficientes butacas vacías.")
+            print("La cantidad de butacas vacías es:", ButacasVacias, ". Ingrese un número menor o igual a este.")
+        NumeroDeButacas = int(input("¿Cuántas butacas desea comprar?: "))
+
+    asientos_reservados = []
+
+    while NumeroDeButacas >= 1:
+        print()
+        print("Seleccione la ubicación de su asiento:")
+        FilaDeLaButaca = int(input("Fila (1-5): ")) - 1
+        ColumnaDeLaButaca = int(input("Columna (1-5): ")) - 1
+
+        ubicacion_valida = (FilaDeLaButaca >= 0 and FilaDeLaButaca < 5) and (ColumnaDeLaButaca >= 0 and ColumnaDeLaButaca < 5)
+        if ubicacion_valida:
+            if Sala[FilaDeLaButaca][ColumnaDeLaButaca] == 0:
+                Sala[FilaDeLaButaca][ColumnaDeLaButaca] = 1
+                print()
+                print("Asiento reservado: Fila", FilaDeLaButaca + 1, "   Columna", ColumnaDeLaButaca + 1)
+                print()
+                asientos_reservados.append((FilaDeLaButaca + 1, ColumnaDeLaButaca + 1))
+                NumeroDeButacas -= 1
+            else:
+                print()
+                print(" Ese asiento ya está ocupado. Elija otro, por favor.")
+                print()
+        else:
+            print()
+            print(" Eleccion erronea. Fila y columna deben estar entre 1 y 5.")
+            print()
+
+    print()
+    print("Resumen de asientos reservados:")
+    for fila, col in asientos_reservados:
+        print(" - Fila", fila, " Columna", col)
+    print()
+
+    return asientos_reservados[-1] 
 
 
 def comprobante(dni,pelicula,sucursal,sala,asiento,precio_final,Nombre):
@@ -172,10 +200,9 @@ def cartelera():
         print(peli)
 
 def formato():
-    print("ingrese el numero en el al que correspondo el formato de la pelicula que quiere ver ")
-    print("1. 2D")
-    print("2. 3D")
-    print("3. 4D")
+    print("2D")
+    print("3D")
+    print("4D")
     formato = int(input("Seleccione el formato de la película (1-3): "))
     while formato < 1 or formato > 3:
         formato = int(input("Por favor, seleccione un formato válido (1-3): "))
@@ -186,16 +213,15 @@ def formato():
     else:
         return "4D"
 
-     
-
 def FinDelDia(Sucursales):
+    recaudacion = []
     SCaballito=0
     SPalermo=0
     SAbasto=0
 
     print("finalizo el dia")
     print("estos son los datos de todas la sucursales")
-            
+
     for i in range(len(Sucursales)):
         for j in range(len(Sucursales[i])):
             for k in range(len(Sucursales[i][j])):
@@ -205,11 +231,11 @@ def FinDelDia(Sucursales):
                                 SAbasto+=7500
                         elif i == 1:
                             if Sucursales[i][j][k][l]==1:
-                                SCaballito+=7500                            
+                                SCaballito+=7500
                         else:
                             if Sucursales[i][j][k][l]==1:
                                 SPalermo+=7500
-
-    print("plata conseguida en el dia en la sucursal abasto:",SAbasto)
-    print("plata conseguida en el dia en la sucursal Caballito:",SCaballito)
-    print("plata conseguida en el dia en la sucursal Palermo:",SPalermo)
+    recaudacion.append(SAbasto,SCaballito,SPalermo)
+    print("plata conseguida en el dia en la sucursal abasto:",recaudacion[0:1])
+    print("plata conseguida en el dia en la sucursal Caballito:",SCaballito[1:2])
+    print("plata conseguida en el dia en la sucursal Palermo:",SPalermo[2:3])
